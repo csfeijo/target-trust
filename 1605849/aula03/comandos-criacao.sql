b27a902c808e2fbc4c1677d37e3c6ea997ffee97
Cicero Feijo <cicero.feijo@hp.com>
2020-11-19 22:11:29 -0300

    [1605849] Diagrama EMPRESA e CREATE DATABASE

---
+++ 1605849/aula03/comandos-criacao.sql
@@ -0,0 +1,9 @@
+# criação de uma base de dados
+-- outro comentario
+# database => schema (nome mais moderno)
+CREATE DATABASE aula;
+
+# CTRL + ENTER executa a linha que esta o cursor
+SHOW DATABASES;
+
+DROP DATABASE aula;
